8643cb1d2dc1c7ff39e57ae01352597ec8cd82c1
Tushar Soni <tusharsoni330gmail.com>
2024-01-26 15:09:01 +0530

    new Reverse Queue

--- src/ReverseQueue.java
+++ src/ReverseQueue.java
@@ -1,38 +1,32 @@
-public class ReverseQueue implements Queue{
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
+public class ReverseQueue {
 
-    @Override
-    public int size() {
-        return 0;
+    public static Queue<Integer> reverseQueue(Queue<Integer> queue){
+        Stack<Integer> stack = new Stack<>();
+        while (!queue.isEmpty()){
+            stack.push(queue.remove());
+        }
+        while (!stack.isEmpty()){
+            queue.add(stack.pop());
+        }
+        return queue;
     }
-
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    @Override
-    public void addElement(int data) {
-
-    }
-
-    @Override
-    public void removeElememt() {
-
-    }
-
-    @Override
-    public int peek() {
-        return 0;
-    }
-
-    @Override
-    public boolean isFull() {
-        return false;
-    }
-
-    @Override
-    public void printQueue() {
-
+    public static void main(String[] args) {
+        Queue<Integer> q1 = new LinkedList<>();
+        q1.add(1);
+        q1.add(2);
+        q1.add(3);
+        q1.add(4);
+        q1.add(5);
+        q1.add(6);
+        q1.add(7);
+        q1.add(8);
+        q1.add(9);
+        q1.add(10);
+        Queue<Integer> reverse = reverseQueue(q1);
+        System.out.println(reverse);
     }
 }
